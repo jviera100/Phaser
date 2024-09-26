538cd31eef473c945121959fd9d545877bd040a1
Jonathan Viera <156127051+jviera100@users.noreply.github.com>
2024-09-26 16:52:39 -0300

    :tada: phaser

--- src/init.js
+++ src/init.js
@@ -3,23 +3,29 @@ const { Physics } = require("phaser");
 // init.js
 const config = {
     title: "Hola Phaser",
-    type: Phaser.AUTO,
-    parent: "contenedor",
-    width: 800,
-    height: 600,
-    scene: {
-        preload,
-        create,
-        update,
-    }, 
-    Physics:{
-        default: 'arcade',
-        arcade: {
-            gravity: {y: 500},
-            debug: false
+    scale:{
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        type: Phaser.AUTO,
+        parent: "contenedor",
+        width: 800,
+        height: 600,
+        scene: {
+            preload,
+            create,
+            update,
+        }, 
+        Physics:{
+            default: 'arcade',
+            arcade: {
+                gravity: {y: 500},
+                debug: false
 
-        }   
+            }   
+        }
+
     }
+    
 }
 
 var game = new Phaser.Game(config);
@@ -28,7 +34,9 @@ function preload (){
     this.load.setBaseUrl("http://labs.phaser.io");
     this.load.image("fondo", "assets/skies/nebula.jpg");
     this.load.image("planeta", "assets/sprites/phaser1.png");
-    this.load.image('particula','assets/particles/yellow.png')    
+    this.load.image('particula','assets/particles/yellow.png');
+    this.Load.image('raqueta', '../Assets/raqueta.png');
+    this.load.image('sonido', '../Assets/audio.mp3');
 };
 function create (){
     console.log(this.add.image(400, 300, "fondo"));
@@ -40,15 +48,28 @@ function create (){
         });
     Mundo = this.physics.add.image(400, 250, 'planeta');
     console.log(Mundo);
-    Mundo.setAlpha(0.6); //class image
+    Mundo.setAlpha(0.6); //class image transparencia
     Mundo.setAngle(45); //class image
     Mundo.setScale(0.7); //class image
     Mundo.setFlipx(true); //class image
     Mundo.setOrigin(0.5, 0.5); //columna - fila  //class image
-    Mundo.setCollideWorldBounds(true); // class physics
+    Mundo.setCollideWorldBounds(true); // class physics gravedad
     Mundo.setBounce(1.5); // class physics
     Mundo.setVelocityX(300, 400); // class physics
     emitter.starFollow(Mundo); // class physics
+    const texto = this.add.text(300, 550, 'Hola Mundo',{
+        color: 'green',
+        fontSize: '20',
+        backgroundColor: 'white',
+        padding: {
+            top: 15,
+            bottom: 15,
+            left: 15,
+            right: 15,
+        },
+        align: 'center',
+    });  
+    texto.alpha= 0.5; 
 };
 function update(time, delta){
     Mundo.angle+= 5;
